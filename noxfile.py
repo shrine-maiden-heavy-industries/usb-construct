a2f75689280771f182b5b9698ae0684903027f32
Aki Van Ness <aki@lethalbit.net>
2025-07-10 16:29:00 -0700

    noxfile: Updated noxfile to match with Torii

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,16 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-import shutil
 from os             import getenv
 from pathlib        import Path
-from setuptools_scm import get_version, ScmVersion
-
 import nox
 from nox.sessions   import Session
 
 ROOT_DIR  = Path(__file__).parent
 
-BUILD_DIR = (ROOT_DIR  / 'build')
-CNTRB_DIR = (ROOT_DIR  / 'contrib')
-DOCS_DIR  = (ROOT_DIR  / 'docs')
-DIST_DIR  = (BUILD_DIR / 'dist')
+BUILD_DIR = ROOT_DIR  / 'build'
+CNTRB_DIR = ROOT_DIR  / 'contrib'
+DOCS_DIR  = ROOT_DIR  / 'docs'
+DIST_DIR  = BUILD_DIR / 'dist'
 
 IN_CI           = getenv('GITHUB_WORKSPACE') is not None
 ENABLE_COVERAGE = IN_CI or getenv('USB_CONSTRUCT_TEST_COVERAGE') is not None
@@ -22,111 +19,103 @@ ENABLE_COVERAGE = IN_CI or getenv('USB_CONSTRUCT_TEST_COVERAGE') is not None
 nox.options.sessions = (
 	'test',
 	'lint',
-	'typecheck'
+	'typecheck-mypy'
 )
-
-def usb_construct_version() -> str:
-	def scheme(version: ScmVersion) -> str:
-		if version.tag and not version.distance:
-			return version.format_with('')
-		else:
-			return version.format_choice('+{node}', '+{node}.dirty')
-
-	return get_version(
-		root           = str(ROOT_DIR),
-		version_scheme = 'guess-next-dev',
-		local_scheme   = scheme,
-		relative_to    = __file__
-	)
 
 @nox.session(reuse_venv = True)
 def test(session: Session) -> None:
-	out_dir = (BUILD_DIR / 'tests')
-	out_dir.mkdir(parents = True, exist_ok = True)
+	OUTPUT_DIR = BUILD_DIR / 'tests'
+	OUTPUT_DIR.mkdir(parents = True, exist_ok = True)
 
 	unitest_args = ('-m', 'unittest', 'discover', '-s', str(ROOT_DIR))
 
 	session.install('.')
+
 	if ENABLE_COVERAGE:
 		session.log('Coverage support enabled')
 		session.install('coverage')
-		coverage_args = (
-			'-m', 'coverage', 'run',
-			f'--rcfile={CNTRB_DIR / "coveragerc"}'
-		)
+		coverage_args = ('-m', 'coverage', 'run', '-p', f'--rcfile={ROOT_DIR / "pyproject.toml"}',)
+		session.env['COVERAGE_CORE'] = 'sysmon'
 	else:
-		coverage_args = ()
+		coverage_args = tuple[str]()
 
-	session.chdir(str(out_dir))
-	session.run(
-		'python', *coverage_args, *unitest_args, *session.posargs
-	)
+	with session.chdir(OUTPUT_DIR):
+		session.log('Running core test suite...')
+		session.run('python', *coverage_args, *unitest_args, *session.posargs)
 
-	if ENABLE_COVERAGE:
-		session.run(
-			'python', '-m', 'coverage', 'xml',
-			f'--rcfile={CNTRB_DIR / "coveragerc"}'
-		)
+		if ENABLE_COVERAGE:
+			session.log('Combining Coverage data..')
+			session.run('python', '-m', 'coverage', 'combine')
+
+			session.log('Generating XML Coverage report...')
+			session.run('python', '-m', 'coverage', 'xml', f'--rcfile={ROOT_DIR / "pyproject.toml"}')
+
+@nox.session(name = 'watch-docs')
+def watch_docs(session: Session) -> None:
+	OUTPUT_DIR = BUILD_DIR / 'docs'
+
+	session.install('-r', str(DOCS_DIR / 'requirements.txt'))
+	session.install('sphinx-autobuild')
+	session.install('.')
+
+	session.run('sphinx-autobuild', str(DOCS_DIR), str(OUTPUT_DIR))
 
 @nox.session(name = 'build-docs')
 def build_docs(session: Session) -> None:
-	out_dir = (BUILD_DIR / 'docs')
-	shutil.rmtree(out_dir, ignore_errors = True)
+	OUTPUT_DIR = BUILD_DIR / 'docs'
+
 	session.install('-r', str(DOCS_DIR / 'requirements.txt'))
 	session.install('.')
-	session.run('sphinx-build', '-b', 'html', str(DOCS_DIR), str(out_dir))
+
+	session.run('sphinx-build', '-b', 'html', str(DOCS_DIR), str(OUTPUT_DIR))
 
 @nox.session(name = 'linkcheck-docs')
 def linkcheck_docs(session: Session) -> None:
-	out_dir = (BUILD_DIR / 'docs-linkcheck')
-	shutil.rmtree(out_dir, ignore_errors = True)
+	OUTPUT_DIR = BUILD_DIR / 'docs-linkcheck'
+
 	session.install('-r', str(DOCS_DIR / 'requirements.txt'))
 	session.install('.')
-	session.run('sphinx-build', '-b', 'linkcheck', str(DOCS_DIR), str(out_dir))
 
-@nox.session
-def typecheck(session: Session) -> None:
-	out_dir = (BUILD_DIR / 'mypy')
-	out_dir.mkdir(parents = True, exist_ok = True)
+	session.run('sphinx-build', '-b', 'linkcheck', str(DOCS_DIR), str(OUTPUT_DIR))
+
+@nox.session(name = 'typecheck-mypy')
+def typecheck_mypy(session: Session) -> None:
+	OUTPUT_DIR = BUILD_DIR / 'typing' / 'mypy'
+	OUTPUT_DIR.mkdir(parents = True, exist_ok = True)
 
 	session.install('mypy')
 	session.install('lxml')
-	session.install('construct-typing') # Stubs for Construct
 	session.install('.')
+
 	session.run(
 		'mypy', '--non-interactive', '--install-types', '--pretty',
-		'--cache-dir', str((out_dir / '.mypy-cache').resolve()),
-		'--config-file', str((CNTRB_DIR / '.mypy.ini').resolve()),
-		'-p', 'usb_construct', '--html-report', str(out_dir.resolve())
+		'--disallow-any-generics',
+		'--cache-dir', str((OUTPUT_DIR / '.mypy-cache').resolve()),
+		'-p', 'usb_construct', '--html-report', str(OUTPUT_DIR.resolve())
 	)
+
+@nox.session(name = 'typecheck-pyright')
+def typecheck_pyright(session: Session) -> None:
+	OUTPUT_DIR = BUILD_DIR / 'typing' / 'pyright'
+	OUTPUT_DIR.mkdir(parents = True, exist_ok = True)
+
+	session.install('pyright')
+	session.install('.')
+
+	with (OUTPUT_DIR / 'pyright.log').open('w') as f:
+		session.run('pyright', *session.posargs, stdout = f)
 
 @nox.session
 def lint(session: Session) -> None:
 	session.install('flake8')
+
 	session.run(
-		'flake8', '--config', str((CNTRB_DIR / '.flake8').resolve()), './usb_construct'
-	)
-	session.run(
-		'flake8', '--config', str((CNTRB_DIR / '.flake8').resolve()), './tests'
-	)
-	session.run(
-		'flake8', '--config', str((CNTRB_DIR / '.flake8').resolve()), './examples'
+		'flake8', '--config', str((CNTRB_DIR / '.flake8').resolve()),
+		'./usb_construct', './tests', './examples', './docs'
 	)
 
 @nox.session
 def dist(session: Session) -> None:
 	session.install('build')
-	session.run(
-		'python', '-m', 'build',
-		'-o', str(DIST_DIR)
-	)
 
-@nox.session
-def upload(session: Session) -> None:
-	session.install('twine')
-	dist(session)
-	session.log(f'Uploading usb-construct-{usb_construct_version()} to PyPi')
-	session.run(
-		'python', '-m', 'twine', 'upload',
-		f'{DIST_DIR}/usb_construct-{usb_construct_version()}*'
-	)
+	session.run('python', '-m', 'build', '-o', str(DIST_DIR))
